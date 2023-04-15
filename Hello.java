c26759999f248f228c194c0c4370e39d712791b1
João Victor Serra <jserralagares@gmail.com>
2023-04-15 14:26:35 -0300

    1.que a variavel  da valor de A, B e C  2. printa na tela os respectivos valores númericos  3. mudar os respectivos valores das letras e mudalas para os alternativos valores  4. e printalos na tela

--- Hello.java
+++ Hello.java
@@ -3,6 +3,23 @@
  */
 public class Hello {
     public static void main(String[] args) {
-        System.out.print("Hello World");
+        int A, B, C;
+
+
+        A = 5;
+        B = 15;
+        C = 10;
+
+        System.out.println(B); // 15
+        System.out.println(C); // 10
+        System.out.println(A); // 5
+
+        A = B;
+        B = A;
+        C = C;
+
+        System.out.println(B); // 15
+        System.out.println(A); // 15
+        System.out.println(C); // 10
     }
 }
