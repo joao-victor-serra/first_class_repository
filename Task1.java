f52f172bd2765888a0e7bafa8a0af129fe805c8e
João Victor Serra <jserralagares@gmail.com>
2023-04-20 16:45:55 -0300

    progamação para a conta dar -56

---
+++ Task1.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class Task1 {
+private static final String B = null;
+private static final String C = null;
+private static final String A = null;
+private static final Object E = null;
+public static void main(String[] args) {
+    final int inputScanner = String (A, B, C, E);
+    
+    int  A, B, C, E;
+    
+    System.out.println( A = 29);
+    System.out.println( B = 2);
+    System.out.println( C = 1);
+    System.out.println( E = 114);
+
+    System.out.println(A*B-E*C);
+}
+
+private static int String(String a2, String b2, String c2, Object e2) {
+    return 0;
+}
+    
+} 
